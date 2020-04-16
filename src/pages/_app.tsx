b13b2d361f542b2b4b738142a808040defbfc30d
Alex <1187547+alexruzzarin@users.noreply.github.com>
2020-04-16 03:15:01 -0400

    fix: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,24 @@ export default class CovidApp extends App {
             content={`${basics.name} | ${basics.label} @ ${actualWork.company}`}
           />
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <link
             href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap"
             rel="stylesheet"
           />
